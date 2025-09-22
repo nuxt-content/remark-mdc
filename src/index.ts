1cda4d8200bb700212ce06d7d7ae9f4c0b77697d
Farnabaz <farnabaz@gmail.com>
2025-09-22 16:48:59 +0200

    types: export plugin options type

--- src/index.ts
+++ src/index.ts
@@ -126,3 +126,4 @@ function bindData(data: Record<string, any>) {
 export { stringifyFrontMatter, parseFrontMatter } from './frontmatter'
 export { default as micromarkExtension } from './micromark-extension'
 export { convertHtmlEntitiesToChars } from './utils'
+export type { RemarkMDCOptions }

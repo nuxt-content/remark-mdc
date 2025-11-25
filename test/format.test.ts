50d9f2e810cd9d31991b8b2432c18d0b0c077036
Farnabaz <farnabaz@gmail.com>
2025-11-25 13:23:28 +0100

    test: add more test in formatting

--- test/format.test.ts
+++ test/format.test.ts
@@ -131,5 +131,69 @@ describe('ul', () => {
         expect(ast.children[0].children.length).toBe(2)
       },
     },
+    list10: {
+      markdown: `---
+title: ""
+description: ""
+---
+
+## This section contains a bunch of lists
+
+- This is a list item.
+- This is a list item.
+  - This is a CHILD list item.
+    - This is a GRANDCHILD list item.
+- This is a list item.
+
+::page-section
+---
+background-image:
+  url: https://images.example.com/dog.png
+color: red
+---
+- This is a list item.
+- This is a list item.
+  - This is a CHILD list item.
+    - This is a GRANDCHILD list item.
+- This is a list item.
+
+  :::container
+  - This is a list item.
+  - This is a list item.
+  - This is a CHILD list item.
+    - This is a GRANDCHILD list item.
+  - This is a list item.
+  :::
+
+  :::container
+  - This is a list item.
+  - This is a list item.
+    - This is a CHILD list item.
+      - This is a GRANDCHILD list item.
+  - This is a list item.
+
+  :button[This is nested another level]
+
+    ::::container
+    ---
+    background-image:
+      url: https://images.example.com/dog.png
+    color: red
+    ---
+    - This is a list item.
+    - This is a list item.
+      - This is a CHILD list item.
+        - This is a GRANDCHILD list item.
+    - This is a list item.
+
+    This content is below the list.
+    ::::
+
+  :icon{name="mdi:github"}
+
+  More outer content.
+  :::
+::`
+    }
   })
 })
